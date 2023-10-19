eff05aac7a7255fe41409772ab72a802c3fe79d0
chandanaredyyyy <reddyofficial.111@gmail.com>
2023-10-19 09:41:09 +0530

    committed 10/19/2023, 9:41:07 AM

--- set02/problem01.c
+++ set02/problem01.c
@@ -21,3 +21,12 @@ void output(float base, float height, float area)
 {
     printf("according to base %f and height %f the area of the triangle is %f",base,height,area);
 }
+int main()
+{
+    float base, height, area;
+    input(&base, &height);
+    find_area(base , height, &area);
+    output( base,  height, area);
+    return 0;
+
+}

f544ccd16a90fbc8cc3983e13139804b536cd76a
chandanaredyyyy <reddyofficial.111@gmail.com>
2023-10-26 16:31:31 +0530

    committed 10/26/2023, 4:31:30 PM

---
+++ set02/problem03.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int input_number();
+int is_composite(int n);
+void output(int n, int result);
+int input_number()
+{
+    printf("Enter the number");
+    scanf("%d",&n);
+    return n;
+}
+int is_composite(int n)
+{
+
+}
+int main()
+{
+    int n;
+}
+
+

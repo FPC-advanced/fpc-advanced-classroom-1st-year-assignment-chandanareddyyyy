8adc92d7c139157b362dc0119f1a78de6d56ee8c
chandanaredyyyy <reddyofficial.111@gmail.com>
2023-10-26 16:43:14 +0530

    committed 10/26/2023, 4:43:12 PM

--- set02/problem03.c
+++ set02/problem03.c
@@ -2,6 +2,14 @@
 int input_number();
 int is_composite(int n);
 void output(int n, int result);
+int main()
+{
+    int n;
+    n=input_number();
+    int composite=is_composite(n);
+    
+    
+}
 int input_number()
 {
     printf("Enter the number");
@@ -10,11 +18,28 @@ int input_number()
 }
 int is_composite(int n)
 {
-
-}
-int main()
+if(n<=0)
 {
-    int n;
+    composite=0;
+}
+else if(n%2==0)
+{
+    composite=1;
+}
+else{
+    composite=0;
+}
+}
+void output(int n, int result)
+{
+    if(composite=1)
+    {
+        printf("the number %d is composite",n);
+    }
+    if(composite=0)
+    {
+        printf("the number %d is not composite",n);
+    }
 }
 
 

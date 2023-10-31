32a44b79539d97730da7b47d149b4b4295aaaaa8
chandanaredyyyy <reddyofficial.111@gmail.com>
2023-10-31 09:00:31 +0530

    committed 10/31/2023, 9:00:30 AM

---
+++ set02/problem04.c
@@ -0,0 +1,15 @@
+int input_array_size();
+void input_array(int n, int a[n]);
+int sum_composite_numbers(int n, int a[n]);
+void output(int sum);
+int input_array_size()
+{
+    printf("input the array size:/n");
+    scanf("%d",n);
+    return n;
+}
+void input_array(int n, int a[n])
+{
+    
+}
+

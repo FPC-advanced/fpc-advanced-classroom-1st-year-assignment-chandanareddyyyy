612a4083f7b429b29421181d0187020d76b7f1bc
chandanaredyyyy <reddyofficial.111@gmail.com>
2023-10-31 09:08:32 +0530

    committed 10/31/2023, 9:08:30 AM

--- set02/problem04.c
+++ set02/problem04.c
@@ -4,12 +4,40 @@ int sum_composite_numbers(int n, int a[n]);
 void output(int sum);
 int input_array_size()
 {
+    int n;
     printf("input the array size:/n");
-    scanf("%d",n);
+    scanf("%d",&n);
     return n;
 }
 void input_array(int n, int a[n])
 {
-    
+    printf("enter the composite numbers of the array:\n");
+    scanf("%d",&a[n]);
+
 }
+int sum_composite_numbers(int n, int a[n])
+{
+    int sum,i;
+    for(i=0;i<=n;i++)
+    {
+        sum=sum+a[i];
+    }
+    return sum;
+}
+void output(int sum)
+{
+    printf("the sum of composite numbers is:\n",sum);
+}
+int main()
+{
+    int a[20],n,sum;
+     n =input_array_size();
+    input_array(n,a);
+    sum=sum_composite_numbers(n,a);
+    output(sum);
+
+return 0;
+
+}
+
 

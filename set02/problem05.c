116fdee9bc54c12772a92ded87558d5482f6a0a4
chandanaredyyyy <reddyofficial.111@gmail.com>
2023-12-01 18:49:37 +0530

    committed 12/1/2023, 6:49:36 PM

--- set02/problem05.c
+++ set02/problem05.c
@@ -11,6 +11,7 @@ int input()
 }
 int find_gcd(int a,int b)
 {
+  int gcd;
   for(int i=0;i<=a&&i<=b;i++)
   {
     if(a%i==0&&b%i==0)
@@ -20,7 +21,7 @@ int find_gcd(int a,int b)
   }
      return gcd;
 }
-void output(int a, int b)
+void output(int a, int b, int gcd)
 {
  printf("The GCD of %d and %d is %d",a,b,gcd);
 }
@@ -29,7 +30,7 @@ int main()
     int a,b,gcd;
     a=input();
     b=input();
-    gcd=find_gcd(a, b, gcd);
+    gcd=find_gcd(a, b);
     output(a, b, gcd);
     return 0; 
 }

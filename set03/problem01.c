007a3042dcab58efc154063e9828ebc21c166195
chandanaredyyyy <reddyofficial.111@gmail.com>
2023-12-05 10:03:51 +0530

    committed 12/5/2023, 10:03:50 AM

--- set03/problem01.c
+++ set03/problem01.c
@@ -4,3 +4,5 @@ void input(float *x1, float *y1, float *x2, float *y2);
 float find_distance(float x1, float y1, float x2, float y2);
 void output(float x1, float y1, float x2, float y2, float distance);
 int main()
+{
+    float x1,x2,y1,y2,dist;

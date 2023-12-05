40ddebde3d70fb3093211e0db144e795321d55e4
chandanaredyyyy <reddyofficial.111@gmail.com>
2023-12-05 10:39:20 +0530

    committed 12/5/2023, 10:39:19 AM

--- set03/problem01.c
+++ set03/problem01.c
@@ -7,5 +7,23 @@ int main()
 {
     float x1,x2,y1,y2,dist;
     input(&x1,&y1,&x2,&y2);
-
-    jkjsf
+     dist=find_distance(x1,y1,x2,y2);
+     output(x1,y1,x2,y2,dist);
+     return 0;
+}
+void input(float *x1, float *y1, float *x2, float *y2)
+{
+    printf("Enter co-ordinates of first point:  ");
+     scanf("%f%f",x1,y1);
+     printf("Enter co-ordinates of second point: ");
+     scanf("%f%f",x2,y2);
+}
+float find_distance(float x1, float y1, float x2, float y2)
+{
+     float dist=sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+    return dist;
+}
+void output(float x1, float y1, float x2, float y2, float distance)
+{
+    
+    

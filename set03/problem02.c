d5d1995434821968b863c3ddbeb06b90a1d9f8af
chandanaredyyyy <reddyofficial.111@gmail.com>
2023-12-05 10:48:20 +0530

    committed 12/5/2023, 10:48:19 AM

--- set03/problem02.c
+++ set03/problem02.c
@@ -12,3 +12,12 @@ int main()
       output(x1,y1,x2,y2,x3,y3,r);
       return 0;
 }
+void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3)
+{
+    printf("Enter co-ordinates of first point :\n");
+    scanf("%f%f",x1,y1);
+    printf("Enter co-ordinates of second point:\n");
+    scanf("%f%f",x2,y2);
+    printf("Enter co-ordinates of third point :\n");
+    scanf("%f%f",x3,y3);
+}

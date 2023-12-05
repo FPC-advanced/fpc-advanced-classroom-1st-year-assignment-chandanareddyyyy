34b932691b798dfce6fe44786d1e61f391cc813b
chandanaredyyyy <reddyofficial.111@gmail.com>
2023-12-05 10:45:20 +0530

    committed 12/5/2023, 10:45:19 AM

--- set03/problem02.c
+++ set03/problem02.c
@@ -3,3 +3,7 @@
 void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3);
 int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3);
 void output(float x1, float y1, float x2, float y2,float x3, float y3, int result);
+int main()
+{
+     float x1,x2,x3,y1,y2,y3;
+     

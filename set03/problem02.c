003bb564f2faea32405981f4f0e71f6f02384cdf
chandanaredyyyy <reddyofficial.111@gmail.com>
2023-12-05 10:44:20 +0530

    committed 12/5/2023, 10:44:19 AM

--- set03/problem02.c
+++ set03/problem02.c
@@ -1,2 +1,4 @@
 #include <stdio.h>
 #include <math.h>
+void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3);
+int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3);
